c03ec746738c7860779c68472012ce5d5797261f
Nadia <nadia_dj@hotmail.co.uk>
2023-01-10 14:09:14 +0000

    actually saved this time

--- calculator.js
+++ calculator.js
@@ -7,9 +7,13 @@ const subtract = function(a,b){
     return a - b;
 };
 
-const multiply = null;
+const multiply = function(a,b) {
+    return a * b;
+};
 
-const divide = null;
+const divide = function(a,b) {
+    return a / b;
+};
 
 const modulus = null;
 
